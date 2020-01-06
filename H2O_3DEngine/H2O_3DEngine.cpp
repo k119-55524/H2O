4ef225cc9fe711c3ee75e4e49a621943063d3a77
Дмитрий <k119_55524@mail.ru>
2020-01-07 01:28:44 +0600

    Добавлены проеткты: 1. Консольный для тестирования и изучения нового в с++. 2. Проект для рабочего стола Windows, который будет использоваться вместе с библиотекой 3D движка. 3. Проект библиотеки 3D движка.
    Signed-off-by: Дмитрий <k119_55524@mail.ru>

---
+++ H2O_3DEngine/H2O_3DEngine.cpp
@@ -0,0 +1,10 @@
+﻿// H2O_3DEngine.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnH2O3DEngine()
+{
+}
